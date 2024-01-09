1cd5204e5991918f0677d904598dcc4c541326eb
Eva-Wamuyu <rutheve.eva@gmail.com>
2024-01-09 19:40:33 +0300

    tailwind config + input form

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserInputComponent } from './user-input/user-input.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserInputComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
